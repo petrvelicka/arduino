7a7691a39fee5f12d363c54fcf7cf3f4b95f123e
Petr Velička <petrvelicka@tuta.io>
2022-07-07 11:31:40 +0200

    morse code decoder

---
+++ morse/morse.ino
@@ -0,0 +1,63 @@
+#define LED 12
+
+#define SERIAL_SPEED 9600
+
+#define DASH 2000
+#define DOT 500
+
+const char *letters[] = {
+	".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+	".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.",
+	"...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
+};
+
+const char *numbers[] = {
+	"-----", ".----", "..---", "...--", "....-", ".....", "-....",
+	"--...", "---..", "----."
+};
+
+void serialPrepare() {
+	Serial.begin(SERIAL_SPEED);
+	Serial.println("Welcome to Arduino morse code encoder");
+}
+
+void setup() {
+	serialPrepare();
+}
+
+void playSignal(int len) {
+	pinMode(LED, HIGH);
+	delay(len);
+	pinMode(LED, LOW);
+	delay(DOT);
+}
+
+void morse(const char *code) {
+	Serial.println(code);
+	unsigned int i = 0;
+	while (code[i]) {
+		int code_len = (code[i] == '.') ? DOT : DASH;
+		playSignal(code_len);
+		i++;
+	}
+}
+
+void loop() {
+	while (Serial.available()) {	
+		char ch;
+		ch = Serial.read();
+		if ('A' <= ch && ch <= 'Z') { // capital letters
+			morse(letters[ch - 'A']);
+		}
+		if ('a' <= ch && ch <= 'z') { // minuscule letters
+			morse(letters[ch - 'a']);
+		}
+		if ('0' <= ch && ch <= '9') { // numbers
+			morse(numbers[ch - '0']);
+		}
+		if (ch == ' ') {
+			delay(DASH); // delay between words
+		}
+	}
+}
+
